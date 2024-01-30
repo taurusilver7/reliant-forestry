22ac57325d1f4b2bf964951d3b8741b7fb06f59e
Devi prasad <prasad20697@gmail.com>
2024-01-30 09:47:45 +0530

    chat component

---
+++ components/chat.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const Chat = () => {
+	return (
+		<div className="z-50 fixed bottom-0 right-0 mr-1 cursor-pointer">
+			<div className="p-2 text-lg text-lime-200 bg-emerald-600 flex w-full rounded-t-lg">
+				<a
+					target="_blank"
+					href="https://direct.lc.chat/17064096/"
+					rel="nofollow"
+				>
+					Chat with us
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Chat;
